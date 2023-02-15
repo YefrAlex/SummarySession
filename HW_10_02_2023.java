4f4c2dd7d9ba8645b0162f3389dbc179b36d2c4f
YefrAlex <117731121+YefrAlex@users.noreply.github.com>
2023-02-15 11:32:10 +0100

    Add files via upload

--- HW_10_02_2023.java
+++ HW_10_02_2023.java
@@ -13,10 +13,20 @@ package SummarySession;
 public class HW_10_02_2023 {
 
     public static void main(String[] args) {
+
+
     }
 
     private class FirmNameList {
         private String firmName;
+
+        public String getFirmName() {
+            return firmName;
+        }
+
+        public void setFirmName(String firmName) {
+            this.firmName = firmName;
+        }
 
         private FirmNameList(String firmName) {
             this.firmName = firmName;
@@ -27,6 +37,30 @@ public class HW_10_02_2023 {
         private String department;
         private int numberOfEmployees;
         private String firmName;
+
+        public String getDepartment() {
+            return department;
+        }
+
+        public int getNumberOfEmployees() {
+            return numberOfEmployees;
+        }
+
+        public String getFirmName() {
+            return firmName;
+        }
+
+        public void setDepartment(String department) {
+            this.department = department;
+        }
+
+        public void setNumberOfEmployees(int numberOfEmployees) {
+            this.numberOfEmployees = numberOfEmployees;
+        }
+
+        public void setFirmName(String firmName) {
+            this.firmName = firmName;
+        }
 
         private Department(String firmName, String department, int numberOfEmployees) {
             super(firmName);
@@ -49,6 +83,38 @@ public class HW_10_02_2023 {
         private int employeeSalary;
         private String department;
 
+        public String getEmployeeName() {
+            return employeeName;
+        }
+
+        public String getEmployeePosition() {
+            return employeePosition;
+        }
+
+        public int getEmployeeSalary() {
+            return employeeSalary;
+        }
+
+        public String getDepartment() {
+            return department;
+        }
+
+        public void setEmployeeName(String employeeName) {
+            this.employeeName = employeeName;
+        }
+
+        public void setEmployeePosition(String employeePosition) {
+            this.employeePosition = employeePosition;
+        }
+
+        public void setEmployeeSalary(int employeeSalary) {
+            this.employeeSalary = employeeSalary;
+        }
+
+        public void setDepartment(String department) {
+            this.department = department;
+        }
+
         private EmployeeList(String department, String employeeName, String employeePosition, int employeeSalary, String firmName) {
             super(department, firmName);
             this.employeeName = employeeName;
@@ -57,6 +123,24 @@ public class HW_10_02_2023 {
             this.department = department;
         }
     }
+    private class EmployeeType extends EmployeeList {
+          private String employeeType;
+
+        public String getEmployeeType() {
+            return employeeType;
+        }
+
+        public void setEmployeeType(String employeeType) {
+            this.employeeType = employeeType;
+        }
+
+        public EmployeeType(String department, String employeeName, String employeePosition, int employeeSalary, String firmName, String employeeType) {
+            super(department, employeeName, employeePosition, employeeSalary, firmName);
+            this.employeeType = employeeType;
+        }
+    }
+
+
 }
 
 
